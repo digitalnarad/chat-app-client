55bf713f9e50ec3e8c45149a74789e339f299dce
Umesh Gurjar <digitalnarad.tech@gmail.com>
2025-07-17 13:35:18 +0530

    popup

--- src/page/Chat/AddNewChat/index.jsx
+++ src/page/Chat/AddNewChat/index.jsx
@@ -1,5 +1,6 @@
 import { Button, Modal } from "react-bootstrap";
 import "./AddNewChat.css";
+import { Search, X } from "lucide-react";
 
 function AddNewChat({ show, onHide }) {
   return (
@@ -12,14 +13,43 @@ function AddNewChat({ show, onHide }) {
       className="add-new-chat-modal"
     >
       <div className="add-new-chat-content">
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-
-        <Button onClick={onHide}>Close</Button>
+        <div className="new-chat-header">
+          <h4 className="modal-title">Message request</h4>
+          <button className="new-chat-close-button" onClick={onHide}>
+            <X size={20} />
+          </button>
+        </div>
+        <div className="sent-request-body">
+          <p className="sent-request-text">
+            You have sent a new message request than search with user name.
+          </p>
+          <div className="sent-request-search-box">
+            <label
+              htmlFor="request_search"
+              className="sent-request-search-label"
+            >
+              <Search size={18} strokeWidth={2} />
+            </label>
+            <input
+              type="text"
+              name="request_search"
+              className="sent-request-search-input"
+              placeholder="Search or sent new chat request."
+            />
+          </div>
+          <div className="sent-request-list chat-scroll">
+            <div className="sent-request-item">
+              <div className="sent-request-avatar">AB</div>
+              <div className="sent-request-info">
+                <div className="sent-request-name">Alice Brown</div>
+                <div className="sent-request-status">user_name</div>
+              </div>
+              <Button variant="primary" size="sm" className="accept-btn">
+                Accept
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </Modal>
   );
